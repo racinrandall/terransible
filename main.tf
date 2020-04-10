6b218e3ac4bbbaa2a6348c85ccdce3781a7336db
Charles Randall <racinrandall@gmail.com>
2020-04-10 11:19:13 -0500

    created security groups

--- main.tf
+++ main.tf
@@ -210,3 +210,109 @@ resource "aws_route_table_association" "wp_private2_assoc" {
   subnet_id      = "${aws_subnet.wp_private2_subnet.id}"
   route_table_id = "${aws_default_route_table.wp_private_rt.id}"
 }
+
+# security groups
+
+resource "aws_security_group" "wp_dev_sg" {
+  name = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+  
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+   
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Public Security Group
+
+resource "aws_security_group" "wp_public_sg" {
+  name = "wp_public_sg"
+  description = "used for the elastic load balancer for public access"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP
+  
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Private Security Group
+
+resource "aws_security_group" "wp_private_sg" {
+  name = "wp_private_sg"
+  description = "used for the private access"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # Access from VPC
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# RDS Security Group
+resource "aws_security_group" "wp_rds_sg" {
+  name = "wp_rds_sg"
+  description = "used for the rds instances access"
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # SQL acces from public/private security groups
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}"
+    ]
+  }
+}
+
+
+
+
+
+ 
